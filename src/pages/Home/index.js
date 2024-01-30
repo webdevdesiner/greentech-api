df1bfb44d45ad0f0267f2887f157f750fecefe45
FERNANDO DE OLIVEIRA GUEDES <fernando.oguedes@senacsp.edu.br>
2024-01-29 22:22:23 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import './style.css';
+
+import React from 'react'
+
+const Home = () => {
+  return (
+        <div>
+            Home
+            
+        
+        </div>
+  )
+}
+
+export default Home

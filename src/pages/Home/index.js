7fd41fe5b8c23e9230af894419307409e5d4a10b
FERNANDO DE OLIVEIRA GUEDES <fernando.oguedes@senacsp.edu.br>
2024-01-30 22:02:06 -0300

    pagina home feita 30 janeiro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,10 +5,18 @@ import React from 'react'
 const Home = () => {
   return (
         <div className='homeArea'>
+          <div className='home'>
             <div className='imgArea'></div>
-       
+            <div className='welcome'><h1>SEJA BEM VINDO</h1></div>  
+            
+            <div className='botao'><button><strong>ENTRAR</strong></button>  </div>
+            <h5 className='direitos'>PROJETO INTEGRADOR: FERNANDO GUEDES - TIMOTIO - MURILO CALABREZO </h5>  
+            
+        </div>
         
         </div>
+
+        
   )
 }
 
